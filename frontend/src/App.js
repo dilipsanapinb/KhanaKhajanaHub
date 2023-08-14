17b44d0c9a709cb83f9252cf513db0b08b89ad8c
DILIP SANAP <dilipinbsanap@gmail.com>
2023-08-15 01:25:57 +0530

    Basic react app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import{BrowserRouter as Router,Routes,Route} from "react-router-dom"
+function App() {
+  return (
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
