d271eb6d734b5664e04f252853904da2804d0606
DILIP SANAP <dilipinbsanap@gmail.com>
2023-08-17 12:13:50 +0530

    recipe details by id  page added

--- frontend/src/components/Recipe/RecipeDetails.jsx
+++ frontend/src/components/Recipe/RecipeDetails.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Badge, Box, Container, Flex, Heading, Icon, Image, List, ListIcon, ListItem, SimpleGrid, Stack, Text } from '@chakra-ui/react';
+import { Badge, Box, Container, Flex, Heading, Icon, Image, SimpleGrid, Stack, Text } from '@chakra-ui/react';
 import { MdCheckCircle } from 'react-icons/md';
+import { FcLikePlaceholder } from 'react-icons/fc'
+import { RiHeartPulseFill } from 'react-icons/ri'
+import { FcAlarmClock } from 'react-icons/fc'
+import { IoLeafSharp } from 'react-icons/io5'
+import{LuSquareDot} from 'react-icons/lu'
+import { GiWheat } from 'react-icons/gi'
+import{LuIndianRupee} from 'react-icons/lu'
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 const RecipeDetails = () => {
     const { id } = useParams();
     const [recipe, setRecipe] = useState({});
-    const [ingredient, setIngredient] = useState([])
+    
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get(`http://localhost:8000/recipe/${id}`);
@@ -25,7 +32,7 @@ const RecipeDetails = () => {
             </nav>
             <main>
                 <Container
-                    maxW={'xl'}
+                    maxW={'xxl'}
                     py={8}
                     width={'90%'}
                     margin={'auto'}
@@ -39,6 +46,7 @@ const RecipeDetails = () => {
                         <Heading
                             as={'h1'}
                             textAlign={'center'}
+                            fontSize="2xl"
                             mb={4}
                         >
                             {recipe.title}
@@ -46,60 +54,189 @@ const RecipeDetails = () => {
 
                         {/* Image of dish */}
                         <Box position="relative" width="100%">
-                        <Image
-                            src={recipe.image}
-                            alt={recipe.title}
-                            boxSize={'400px'}
-                            borderRadius={'10px'}
-                            mx={'auto'}
-                        />
-                        {/* Badgeof dish */}
-                        <Badge
-                            colorScheme="teal"
-                            fontSize="sm"
-                            fontWeight="semibold"
-                            position={'absolute'}
-                            bottom="0"
-                            right="20"
-                            m={2}
-                            p={1}
-                            zIndex="1"
-                        >
-                            
-                            {recipe.aggregateLikes} Likes
-                        </Badge>
+                            <Image
+                                src={recipe.image}
+                                alt={recipe.title}
+                                boxSize={'500px'}
+                                borderRadius={'10px'}
+                                mx={'auto'}
+                            />
+                            {/* Badgeof dish */}
+                            <Badge
+                                colorScheme="teal"
+                                fontSize="sm"
+                                fontWeight="semibold"
+                                position={'absolute'}
+                                bottom="0"
+                                right="20"
+                                m={2}
+                                p={1}
+                                zIndex="1"
+                            >
+                                <Flex align={'center'}>
+                                    <Icon as={FcLikePlaceholder} boxSize={4} mr={'auto'} />
+                                    {recipe.aggregateLikes} Likes
+                                </Flex>
+                                
+                            </Badge>
                         </Box>
                     </Stack>
-                    <Stack
-                        mt={4}
-                        spacing={2}
-                    >
-                        <Text fontSize="lg">
-                            <strong>Health Score:</strong> {recipe.healthScore}
-                        </Text>
-                        <Text fontSize="lg">
-                            <strong>Ready in Minutes:</strong> {recipe.readyInMinutes}
-                        </Text>
-                        <Text fontSize="lg">
-                            <strong>Vegan:</strong> {recipe.vegan ? 'Yes' : 'No'}
-                        </Text>
-                        <Text fontSize="lg">
-                            <strong>Gluten-Free:</strong> {recipe.glutenFree ? 'Yes' : 'No'}
-                        </Text>
-                        <Text fontSize="lg">
-                            <strong>Vegetarian:</strong> {recipe.vegetarian ? 'Yes' : 'No'}
-                        </Text>
-                        <Text fontSize="lg">
-                            <strong>Price Per Serving:</strong> {recipe.pricePerServing}
-                        </Text>
 
-                    </Stack>
+                    {/* Information */}
+                    <Box
+                        mt={10}
+                    >
+                        
+                        <SimpleGrid columns={{ base: 2, sm: 3, md: 3 }} spacing={4}>
+                            {/* Health */}
+                            
+                            <Box
+                                borderWidth="1px"
+                                borderRadius="lg"
+                                boxShadow="md"
+                                bg="white"
+                                height={'100px'}
+                                maxW={'auto'}
+                                p={4}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Flex direction="column" alignItems="center" textAlign="center">
+                                    <Icon as={RiHeartPulseFill} color="red.500" boxSize={7} />
+                                    <Text fontSize="lg" fontWeight="bold" mt={2}>
+                                        Health
+                                    </Text>
+                                    {recipe.healthScore}
+                                </Flex>
+                            </Box>
+                            
+                            {/* preparation time */}
+                            <Box
+                                borderWidth="1px"
+                                borderRadius="lg"
+                                boxShadow="md"
+                                bg="white"
+                                height={'100px'}
+                                maxW={'auto'}
+                                p={4}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Flex direction="column" alignItems="center" textAlign="center">
+                                    <Icon as={FcAlarmClock} color="green.500" boxSize={7} />
+                                    <Text fontSize="lg" fontWeight="bold" mt={2}>
+                                        Time
+                                    </Text>
+                                    <Text fontSize="md">
+                                        {recipe.readyInMinutes} mins
+                                    </Text>
+                                </Flex>
+                            </Box>
+
+                            {/* Vegan */}
+                            <Box
+                                borderWidth="1px"
+                                borderRadius="lg"
+                                boxShadow="md"
+                                bg="white"
+                                height={'100px'}
+                                maxW={'auto'}
+                                p={4}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Flex direction="column" alignItems="center" textAlign="center">
+                                    <Icon as={IoLeafSharp} color="green.500" boxSize={7} />
+                                    <Text fontSize="lg" fontWeight="bold" mt={2}>
+                                        Vegan
+                                    </Text>
+                                    <Text fontSize="md">
+                                        {recipe.vegan ? 'Yes' : 'No'}
+                                    </Text>
+                                </Flex>
+                            </Box>
+
+                            {/* Gluten free */}
+                            <Box
+                                borderWidth="1px"
+                                borderRadius="lg"
+                                boxShadow="md"
+                                bg="white"
+                                height={'100px'}
+                                maxW={'auto'}
+                                p={4}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Flex direction="column" alignItems="center" textAlign="center">
+                                    <Icon as={GiWheat} color="green.500" boxSize={7} />
+                                    <Text fontSize="lg" fontWeight="bold" mt={2}>
+                                        Gluten free
+                                    </Text>
+                                    <Text fontSize="md">
+                                        {recipe.vegan ? 'Yes' : 'No'}
+                                    </Text>
+                                </Flex>
+                            </Box>
+
+                            <Box
+                                borderWidth="1px"
+                                borderRadius="lg"
+                                boxShadow="md"
+                                bg="white"
+                                height={'100px'}
+                                maxW={'auto'}
+                                p={4}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Flex direction="column" alignItems="center" textAlign="center">
+                                    <LuSquareDot
+                                        color={recipe.vegetarian ? 'green' : 'red'}
+                                        size={30}
+                                    />
+                                    <Text fontSize="lg" fontWeight="bold" mt={2}>
+                                        {recipe.vegetarian ? 'Veg' : 'Non-Veg'}
+                                    </Text>
+                                </Flex>
+                            </Box>
+                            {/*Cost per serving*/}
+                            <Box
+                                borderWidth="1px"
+                                borderRadius="lg"
+                                boxShadow="md"
+                                bg="white"
+                                height={'100px'}
+                                maxW={'auto'}
+                                p={4}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Flex direction="column" alignItems="center" textAlign="center">
+                                    <Icon as={LuIndianRupee} color="green.500" boxSize={7} />
+                                    <Text fontSize="lg" fontWeight="bold" mt={2}>
+                                        Cost
+                                    </Text>
+                                    <Text fontSize="md">
+                                        {recipe.pricePerServing}
+                                    </Text>
+                                </Flex>
+                            </Box>
+                        </SimpleGrid>
+                    </Box>
+                    
                     {/* Ingredients */}
                     <Box
-                        width={'120%'}
+                        width={'90%'}
                         margin={'auto'}
                     >
-                        <Text fontSize="xl" fontWeight="bold" mt={6}>
+                        <Text fontSize="xl" fontWeight="bold" mt={10} mb={'6'}>
                             Ingredients:
                         </Text>
                         <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} spacing={4}>
@@ -124,6 +261,39 @@ const RecipeDetails = () => {
                                     <Text fontSize="sm">
                                         Unit: {ingredient.unit}
                                     </Text>
+                                </Box>
+                            ))}
+                        </SimpleGrid>
+                    </Box>
+                    {/* Ingrediants box end */}
+                    
+                    <Box
+                        width={'90%'}
+                        margin={'auto'}
+                    >
+                        <Text fontSize="xl" fontWeight="bold" mt={10} mb={'6'}>
+                            Cooking Instructions:
+                        </Text>
+                        <SimpleGrid columns={{ base: 1, sm: 1, md: 1 }} spacing={4}>
+                            {recipe.analyzedInstructions && recipe.analyzedInstructions[0].steps.map((step) => (
+                                <Box
+                                    key={step.id}
+                                    p={2}
+                                    borderWidth="1px"
+                                    borderRadius="lg"
+                                    boxShadow="md"
+                                    bg="white"
+                                >
+                                    <Flex align="center" mb={2}>
+                                        <Icon as={MdCheckCircle} color="green.500" boxSize={5} mr={2} />
+                                        <Text fontSize="md" fontWeight="semibold">
+                                            Step: {step.number}
+                                        </Text>
+                                    </Flex>
+                                    <Text fontSize="sm">
+                                        {step.step}
+                                    </Text>
+                                    
                                 </Box>
                             ))}
                         </SimpleGrid>
