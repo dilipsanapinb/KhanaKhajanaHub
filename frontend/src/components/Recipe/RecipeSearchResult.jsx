ac8a852c8f5f3b1d14f4b3873657d776956794c7
DILIP SANAP <dilipinbsanap@gmail.com>
2023-08-18 18:19:27 +0530

    Search functionality implemented fronend

---
+++ frontend/src/components/Recipe/RecipeSearchResult.jsx
@@ -0,0 +1,45 @@
+import { Box,Text,Link as ChakraLink } from '@chakra-ui/layout';
+import { Link as RouterLink } from 'react-router-dom';
+import React from 'react'
+
+const RecipeSearchResult = ({ recipes,isOpen }) => {
+        
+     if (!isOpen || recipes.length === 0) {
+    return null;
+  }
+    return (
+
+        <Box
+            position={'absolute'}
+            zIndex={10}
+            mt={'80px'}
+            top={0}
+            left={0}
+            width={'100%'}
+            bg={'white'}
+            boxShadow={'0px 4px rgba(0, 0, 0,0.1)'}
+            borderRadius={'md'}
+            p={'1rem'}
+            >
+            
+            {recipes.map((recipe) => (
+                <ChakraLink
+                    as={RouterLink}
+                    to={`/recipe/${recipe.id}`}
+                    key={recipe.id}
+                    mb={'0.5rem'}
+                    display={'block'}
+                    _hover={{ bg: 'gray.200' }}
+                >
+                    <Text
+                    fontWeight={'bold'}
+                    >
+                        {recipe.title}
+                    </Text>
+                </ChakraLink>
+            ))}
+        </Box>
+    )
+};
+
+export default RecipeSearchResult;
